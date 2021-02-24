ad3b6954d88b02ea0b33875298a4183807bf23c5
Owen Mowery <owenmowery99@gmail.com>
2021-02-24 16:22:25 -0500

    commit tree e9a7ae4b1f0fc00af1e979b7acf9b2e0bce9ca66

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+require 'pry'
+katz_deli = []
+
+def line(array)
+    if array.size == 0
+        puts "The line is currently empty."
+    
+    else
+        new_array = []
+        array.each_with_index do |name, i|
+            new_array << "#{i + 1}. #{name}"
+        end
+        puts "The line is currently: #{new_array.join(' ')}"
+    end
+end
+
+def take_a_number(array, name)
+    puts "Welcome, #{name}. You are number #{array.size + 1} in line."
+    array << name
+end
+
+def now_serving(array)
+    if array.size == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array[0]}."
+        array.shift
+    end
+end
